b61129349402d7f027a595f5b2edaff9f7e5dead
DSouLzz <sjostedt.d@gmail.com>
2026-08-16 00:20:03 +0200

    Enable PAL-AI activity indicator

--- launcher.py
+++ launcher.py
@@ -9,6 +9,7 @@ from map_overlay import apply_map_overlay
 from map_precision import apply_map_precision
 from performance_patch import apply_performance_patch
 from online_research import apply_online_research
+from activity_indicator import apply_activity_indicator
 
 # Apply runtime patches before the app starts.
 pal_ai.Voice.speak = reliable_speak
@@ -16,6 +17,7 @@ apply_map_overlay(pal_ai)
 apply_map_precision(pal_ai)
 apply_performance_patch(pal_ai)
 apply_online_research(pal_ai)
+apply_activity_indicator(pal_ai)
 
 
 def safe_check_updates(self, silent=False):
